46862e8cadfbb77dd1ffb2cf4d294f1b583a2a96
sky <anvanc37@gmail.com>
2022-01-24 12:39:32 -0500

    finish question 217 Contains Duplicate

---
+++ Easy/E217_ContainsDeuplicate.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+/*
+ https://leetcode.com/problems/contains-duplicate/
+ Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ Example 1:
+
+ Input: nums = [1,2,3,1]
+ Output: true
+ Example 2:
+
+ Input: nums = [1,2,3,4]
+ Output: false
+ Example 3:
+
+ Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+  
+
+ Constraints:
+
+ 1 <= nums.length <= 105
+ -109 <= nums[i] <= 109
+ */
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    return nums.count != Set(nums).count
+}
+
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    var dict = [Int: Int]()
+    for num in nums {
+        dict[num, default: 0] += 1
+        if dict[num]! > 1 {
+            return true
+        }
+    }
+    
+    return false
+}
+
+let nums = [1,2,3,1]
+print(containsDuplicate(nums))
