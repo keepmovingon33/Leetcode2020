a2c81fcadc1ff09e7aca2411d04413b083facd0a
sky <anvanc37@gmail.com>
2022-01-30 23:26:30 -0500

    finish question 69

--- Easy/E0069_Squrt.swift
+++ Easy/E0069_Squrt.swift
@@ -1,6 +1,11 @@
-import UIKit
+import Foundation
 
 /*
+ 
+https://leetcode.com/problems/sqrtx/submissions/
+ 
+Sqrt(x)
+ 
 Given a non-negative integer x, compute and return the square root of x.
 
 Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
@@ -19,3 +24,14 @@ Input: x = 8
 Output: 2
 Explanation: The square root of 8 is 2.82842..., and since the decimal part is truncated, 2 is returned.
 */
+
+func mySqrt(_ x: Int) -> Int {
+    var low = 1, high = x
+    while true {
+        var mid = (low + high) / 2
+        if mid * mid <= x && (mid + 1) * (mid + 1) > x { return mid }
+        else if mid * mid > x { high = mid - 1 }
+        else { low = mid + 1 }
+    }
+    return low
+}
