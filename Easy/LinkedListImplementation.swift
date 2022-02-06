bf16f4100978eddae40fd5641005a0b1749f6b41
sky <anvanc37@gmail.com>
2022-02-05 23:56:38 -0500

    implement double linked list add and count functions

--- Easy/LinkedListImplementation.swift
+++ Easy/LinkedListImplementation.swift
@@ -32,8 +32,37 @@ public class LinkedList<T> {
         
         return node
     }
+    
+    public var count: Int {
+        guard var node = head else { return 0 }
+        var count = 1
+        while let next = node.next {
+            node = next
+            count += 1
+        }
+        
+        return count
+    }
+    
+    public func append(value: T) {
+        let newNode = Node(value: value)
+        if let lastNode = last {
+            // at least one node in the list
+            newNode.previous = lastNode
+            newNode.next = nil
+            lastNode.next = newNode
+        } else {
+            // no nodes in list
+            head = newNode
+        }
+    }
 }
 
 let list = LinkedList<String>()
 list.first
 list.last
+list.count
+list.append(value: "one")
+list.count
+list.append(value: "two")
+list.count
